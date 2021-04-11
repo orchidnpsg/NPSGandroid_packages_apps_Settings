0446cb1f82c0ffc7d0f6f54d4b9553bdcc4f2fb9
orchidaosp <78587755+orchidaosp@users.noreply.github.com>
2021-04-11 09:57:10 +0100

    Revert "Settings: Swipe up on keyguard to use Face Unlock [2/2]"
    This reverts commit 122877b081da8d572a556a2ebd8bb27f26b2c30c.

--- src/com/android/settings/biometrics/face/FaceSettings.java
+++ src/com/android/settings/biometrics/face/FaceSettings.java
@@ -65,7 +65,6 @@ public class FaceSettings extends DashboardFragment {
     private FaceSettingsRemoveButtonPreferenceController mRemoveController;
     private FaceSettingsEnrollButtonPreferenceController mEnrollController;
     private FaceSettingsLockscreenBypassPreferenceController mLockscreenController;
-    private FaceSettingsSwipePreferenceController mSwipeController;
     private List<AbstractPreferenceController> mControllers;
 
     private List<Preference> mTogglePreferences;
@@ -135,7 +134,6 @@ public class FaceSettings extends DashboardFragment {
         Preference confirmPref = findPreference(FaceSettingsConfirmPreferenceController.KEY);
         Preference bypassPref =
                 findPreference(mLockscreenController.getPreferenceKey());
-        Preference swipePref = findPreference(mSwipeController.getPreferenceKey());
         mTogglePreferences = new ArrayList<>(
                 Arrays.asList(keyguardPref, appPref, attentionPref, confirmPref, bypassPref));
 
@@ -156,7 +154,6 @@ public class FaceSettings extends DashboardFragment {
         if (mUserManager.isManagedProfile(mUserId)) {
             removePreference(FaceSettingsKeyguardPreferenceController.KEY);
             removePreference(mLockscreenController.getPreferenceKey());
-            removePreference(mSwipeController.getPreferenceKey());
         }
 
         if (savedInstanceState != null) {
@@ -170,8 +167,6 @@ public class FaceSettings extends DashboardFragment {
 
         mLockscreenController = use(FaceSettingsLockscreenBypassPreferenceController.class);
         mLockscreenController.setUserId(mUserId);
-        mSwipeController = use(FaceSettingsSwipePreferenceController.class);
-        mSwipeController.setUserId(mUserId);
     }
 
     @Override
