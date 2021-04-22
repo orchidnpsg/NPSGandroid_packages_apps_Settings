LiquidRemix/android_packages_apps_Settings

settings: add liquid lounge
Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,8 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.liquid.liquidlounge.LiquidLoungeSettings;
+
 public class SettingsGateway {
 
     /**
@@ -261,6 +263,7 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
+            LiquidLoungeSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -299,5 +302,7 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
+            // LiquidRemix settings
+            Settings.LiquidLoungeSettingsActivity.class.getName(),
     };
 }

orchidnpsg/NPSGandroid_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.extra.settings.ExtraSettings;
 
+import com.liquid.liquidlounge.LiquidLoungeSettings;
+
 public class SettingsGateway {
 
     /**
@@ -286,7 +288,8 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
-            ExtraSettings.class.getName()
+            ExtraSettings.class.getName(),
+            LiquidLoungeSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -326,5 +329,7 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.ExtraSettingsActivity.class.getName(),
+            // LiquidRemix settings
+            Settings.LiquidLoungeSettingsActivity.class.getName()
     };
             }